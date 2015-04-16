114d8bd26d13f3cc7f9d0d8691fe659885f80531
David St-Pierre <davstpierre@MacBookProDeDavidStPierre.local>
2015-04-16 14:12:35 -0400

    Ajout de mes fichiers

--- code/app/src/main/java/com/leva/nick/leva/dataManager/DataManager.java
+++ code/app/src/main/java/com/leva/nick/leva/dataManager/DataManager.java
@@ -4,6 +4,9 @@ package com.leva.nick.leva.dataManager;
 import android.content.Context;
 import android.util.Log;
 
+import com.leva.nick.leva.openingHours.ItemOpeningHours;
+import com.leva.nick.leva.promotions.ItemPromotion;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
@@ -13,16 +16,12 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
 
-import com.leva.nick.leva.openingHours.ItemOpeningHours;
-import com.leva.nick.leva.promotions.ItemPromotion;
-
 /**
  * Created by davstpierre on 15-04-06.
  */
 public class DataManager {
 
     public static void writeOpeningHours(Context context, ArrayList<ItemOpeningHours> listToWrite) {
-        ArrayList<ItemOpeningHours> temp = new ArrayList<ItemOpeningHours>();
         String filename = context.getFilesDir().getPath().toString() + "/OpeningHours.txt";
 
         try {
@@ -107,7 +106,53 @@ public class DataManager {
 
             for (int i = 0 ; i < 7 ; i++) {
                 readList.add(new ItemPromotion(input.readLine(), input.readLine(), input.readLine(),
-                                               input.readLine(), input.readLine(), input.readLine()));
+                        input.readLine(), input.readLine(), input.readLine()));
+            }
+
+            Log.d("IMA", " Fichier lu ");
+            return readList;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void writeEntrepriseAccountDetails(Context context, ArrayList<String> accountDetails,
+                                                     String username) {
+        String filename = context.getFilesDir().getPath().toString() + "/AccountDetails" + username + ".txt";
+
+        try {
+            FileWriter fw = new FileWriter(filename);
+            Writer output = new BufferedWriter(fw);
+
+            for (int i = 0 ; i < accountDetails.size() ; i++) {
+                output.write(accountDetails.get(i).toString() + "\n");
+            }
+
+            output.close();
+            Log.d("IMA", " Fichier écrit");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<String> readEntrepriseAccountDetails(Context context, String username) {
+
+        ArrayList<String> readList = new ArrayList<String>();
+        String filename = context.getFilesDir().getPath().toString() + "/AccountDetails" + username + ".txt";
+
+        try {
+            BufferedReader input = new BufferedReader(new FileReader(filename));
+            if (!input.ready()) {
+                throw new IOException();
+            }
+
+            for (int i = 0 ; i < 8; i++) {
+                readList.add(input.readLine());
             }
 
             Log.d("IMA", " Fichier lu ");
